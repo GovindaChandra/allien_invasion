70de81d0243291fb7eb46cb0239c822629e039e6
Sergey Toylyev <doyalnitay@gmail.com>
2020-04-13 00:37:20 +0500

    Add two classes, setting, ship

---
+++ settings.py
@@ -0,0 +1,9 @@
+class Settings:
+    """Class for storing all settings of the game Alien Invasion"""
+
+    def __init__(self):
+        """Initializes the game settings"""
+        # Parameters of the screen
+        self.screen_width = 1280
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
