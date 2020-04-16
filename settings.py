2d37befa1006510b311617d1cf1dc6daf97d2ce9
Sergey Toylyev <doyalnitay@gmail.com>
2020-04-16 20:57:16 +0500

    Add settings for bullet

--- settings.py
+++ settings.py
@@ -12,4 +12,9 @@ class Settings:
         # Parameters of the ship
         self.ship_speed = 1.5
 
+        # Bullet settings
+        self.bullet_speed = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
 
